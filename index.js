24da81db070482a1bb9b488b12ff2b2b12afad46
Satyam Godara <satyamgodara2005@gmail.com>
2026-04-07 22:10:25 +0530

    folder structure created, and chat app built

---
+++ index.js
@@ -0,0 +1,46 @@
+const http = require('http');
+const express = require('express');
+const path = require('path');
+const {Server} = require('socket.io');
+
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+
+io.on("connection",(socket)=>{
+    socket.on('userEntered',(username)=>{
+        socket.name = username;
+        io.emit("userEntered",username);
+    })
+    socket.on("user-message",(data)=>{
+        const {username,message} = data;
+        if(username && message){
+            io.emit("message",{username,message})
+        }
+    });
+    socket.on('disconnect',()=>{
+        if(socket.name){
+            io.emit('exit',socket.name);
+        }
+    })
+    
+});
+// io.on("disconnect",()=>{
+//     socket.on("exit",(data)=>{
+//         const {username} = data;
+//         io.emit("exit");
+//     })
+// })
+
+
+app.use(express.static(path.resolve("./public")));
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve("./public/index.html"));
+});
+
+server.listen(9000,()=>{
+    console.log("Server running at port : 9000");
+});
